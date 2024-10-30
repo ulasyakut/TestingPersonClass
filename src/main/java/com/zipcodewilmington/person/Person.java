ff68b20a2e3b7a9716d7474f87ce22c349d94d60
Ulas Yakut <uyakut@Zipcoders-MacBook-Pro-5.local>
2024-10-29 21:21:14 -0400

    needs work

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -4,32 +4,65 @@ package com.zipcodewilmington.person;
  * If you did ObjectsOne lab, you might recognize this code?
  */
 public class Person {
-    private String name;
-    private int age;
+    //we have this class named Person
 
-    public Person() {
-    }
+    private String name = "Leon";
+    private int age = 5;
+    //private int salary;
+    //private String company;
+    //private String car;
+    //private String state;
+    //to encapsulate data about object,we declared variables in class as private and
+    //we will write public methods in class to set and get those values
+    //Person person = new Person(name,age);
+
+
+    //public Person (String name,int age) {
+        //this.name = name;
+        //this.age = age;
+        //this.salary = salary;
+        //this.company = company;
+        //this.car = car;
+        //this.state = state;
+    //}
+    Person person = new Person(name,age);
+
+
+
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+
+
     }
 
+
     public void setName(String name) {
+
+        this.name =name;
     }
 
     public void setAge(int age) {
+
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+
+        return person.name;
     }
 
-    public Integer getAge() {
-        return null;
+    public int getAge() {
+
+        return person.age;
     }
 }
